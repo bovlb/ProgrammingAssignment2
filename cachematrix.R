0f3d370e5b2b758eed114cc6f11297b019c236ab
bovlb <github@meninx.com>
2018-09-05 16:08:37 -0700

    First version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Supports caching of matrix inverse.
+## E.g.:
+##   my_matrix <- makeCacheMatrix(matrix(rnorm(1000000), 1000, 1000))
+##   system.time(inverse <- cacheSolve(my_matrix)) # slow
+##   system.time(inverse <- cacheSolve(my_matrix)) # fast
 
-## Write a short comment describing this function
+## makeCacheMatrix: Builds a list object to cache a matrix and its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse 
+    getinv <- function() inv 
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of a caching matrix object returned by makeCacheMatrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+    inv <- m$getinv()
+    if(!is.null(inv)) inv else m$setinv(solve(m$get()))
 }
